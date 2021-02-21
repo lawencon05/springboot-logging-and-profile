c1b4350c1dfc1d5f528edde4958e18feb33279de
lawencon05 <lawencon05@lawencon.com>
2021-02-21 21:08:37 +0700

    first init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+	
+	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public ResponseEntity<?> get(){
+		logger.info("coba logger info");
+		logger.debug("coba logger debug");
+		logger.warn("coba logger warn");
+		logger.error("coba logger error");
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+	
+}
